f0b2796e38889f54dc0de295639c7cd0186f9ef1
italo Souza <italoasouzat.i@gmail.com>
2024-12-15 17:33:57 -0300

    Initial Commit

---
+++ Reminder/Sources/Features/LoginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  LoginBottomSheetViewModel.swift
+//  Reminder
+//
+//  Created by Arthur Rios on 13/10/24.
+//
+
+import Foundation
+import Firebase
+
+class LoginBottomSheetViewModel {
+    func doAuth(usernameLogin: String, password: String) {
+        print(usernameLogin)
+        
+    }
+}
